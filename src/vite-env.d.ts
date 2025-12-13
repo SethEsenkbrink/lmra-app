c6ac366168a5c71f4b9885e9331b23d552d180b9
hoeslaken <izaak.esenkbrink@gmail.com>
2025-12-13 16:19:23 +0100

    fix(pdf): switch to native jspdf generation & fix type definitions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="vite/client" />
+
+// Globale constanten
 declare const __APP_VERSION__: string;
+
+// Module definitie voor jspdf-autotable
+declare module 'jspdf-autotable' {
+    import { jsPDF } from 'jspdf';
+    
+    // We vertellen TypeScript dat deze functie bestaat en een jsPDF instantie + opties verwacht
+    export default function autoTable(doc: jsPDF, options: any): void;
+}
